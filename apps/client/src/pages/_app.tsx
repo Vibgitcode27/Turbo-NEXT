8b2aff0dfef8a7a231abd3f2891b6b9ec4e020e4
vibhor <vibgitcode27@gmail.com>
2023-11-16 02:31:16 +0530

    added logged in checking functionality

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Appbar from '@/components/Appbar.'
-import { RecoilRoot ,useRecoilValue , useRecoilState } from 'recoil'
-import { isUserLoading } from 'store'
+import { RecoilRoot ,useRecoilValue , useRecoilState , useSetRecoilState } from 'recoil'
+import { isUserLoading , userState } from 'store'
+import axios from "axios"
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
   // return <Component {...pageProps} />
@@ -19,10 +21,48 @@ function App2({Component , pageProps} : {Component : any , pageProps : any})
   {
     return <div>
       Loading...s
+      <InitUser/>
     </div>
   }
   return <div>
     <Appbar/>
     <Component {...pageProps} />
   </div>
+}
+
+function InitUser() {
+  const setUser = useSetRecoilState(userState);
+  const init = async() => {
+      try {
+          const response = await axios.get(`/api/auth/me`, {
+              headers: {
+                  "Authorization": "Bearer " + localStorage.getItem("token")
+              }
+          })
+
+          if (response.data.user) {
+              setUser({
+                  isLoading: false,
+                  userEmail: response.data.user.username
+              })
+          } else {
+              setUser({
+                  isLoading: false,
+                  userEmail: null
+              })
+          }
+      } catch (e) {
+
+          setUser({
+              isLoading: false,
+              userEmail: null
+          })
+      }
+  };
+
+  useEffect(() => {
+      init();
+  }, []);
+
+  return <></>
 }
